5b1230cb1cf99f19c64a35d6b202a333c42ce885
Tobi <naroto102hn2@gmail.com>
2020-09-15 17:04:15 +0700

    update 15/09/20

--- session2_var/assignment.js
+++ session2_var/assignment.js
@@ -6,7 +6,15 @@
 // let dai=Number(prompt("Nhap chieu dai: "));
 // let rong=Number(prompt("Nhap chieu rong: "))
 // console.log("Bai 2:");
-// console.log()
+// console.log(`Chu vi la: ${(dai+rong)*2}`);
+// console.log(`Dien tich la: ${dai*rong}`);
+
+let x = Number(prompt("Nhap vao x: "));
+console.log("Bai 3: ");
+let y1 = 4*(x*x + 10*x*Math.sqrt(x)+3*x+1);
+console.log("Gia tri cua y1 = "+y1);
+let y2 = (Math.sin(Math.PI*x*x)+Math.sqrt(x*x+1))/(Math.exp(2*x)+Math.cos((Math.PI)/4*x))
+console.log("Gia tri cua y2 = "+y2);
 
 // let X = Number(prompt("Nhap vao so tien: "));
 // let m=X;
@@ -22,10 +30,10 @@
 // console.log(m + " = " + dem100 +"to100000 +" + dem50+"to50000 +" + dem20+ "to20000 +" + dem10+"to10000");
 
 
-let X=Number(prompt("Nhap vao 1 so: "));
-let m=X;
-let a=Math.floor(X/100);
-X-=a*100;
-let b=Math.floor(X/10);
-X-=b*10;
-console.log(`${m} = ${a} + ${b} + ${X} = `+ (a+b+X));
+// let X=Number(prompt("Nhap vao 1 so: "));
+// let m=X;
+// let a=Math.floor(X/100);
+// X-=a*100;
+// let b=Math.floor(X/10);
+// X-=b*10;
+// console.log(`${m} = ${a} + ${b} + ${X} = `+ (a+b+X));
